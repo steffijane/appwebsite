6da567b88c5aba3ba06cf7f25cfa29f1328ac507
steffijane <balarezos@gmail.com>
2016-02-02 18:55:51 -0500

    Create new app.js

---
+++ new app.js
@@ -0,0 +1,13 @@
+ window.onload = function(){
+  var step=1
+  function slideit(){
+    document.getElementById('my-img').src = 'image'+step+'.jpg'
+    if(step<3)
+      step++
+    else
+      step=1
+    setTimeout(slideit,2500)
+  }
+
+  slideit()
+}
